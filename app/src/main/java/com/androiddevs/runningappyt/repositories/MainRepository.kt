6522110189d09d6f85d4bcd561ea4d52c3924572
Bogdan24 <bogdanlonchuk55@gmail.com>
2021-02-06 23:50:46 +0200

    - Fragments added;

---
+++ app/src/main/java/com/androiddevs/runningappyt/repositories/MainRepository.kt
@@ -0,0 +1,32 @@
+package com.androiddevs.runningappyt.repositories
+
+import com.androiddevs.runningappyt.db.Run
+import com.androiddevs.runningappyt.db.RunDAO
+import javax.inject.Inject
+
+class MainRepository @Inject constructor(
+    val runDAO: RunDAO
+) {
+    suspend fun insertRun(run: Run) = runDAO.insertRun(run)
+    suspend fun deleteRun(run: Run) = runDAO.deleteRun(run)
+
+    fun getAllRunsSortedByDay() = runDAO.getAllRunsSortedByDay()
+
+    fun getAllRunsSortedByDistance() = runDAO.getAllRunsSortedByDistance()
+
+    fun getAllRunsSortedByTimeInMillis() = runDAO.getAllRunsSortedByTimeInMillis()
+
+    fun getAllRunsSortedByAvgSpeed() = runDAO.getAllRunsSortedByAvgSpeed()
+
+    fun getAllRunsSortedByCaloriesBurned() = runDAO.getAllRunsSortedByCaloriesBurned()
+
+    fun getTotalDistance() = runDAO.getTotalDistanceInMeters()
+
+    fun getTotalCalories() = runDAO.getTotalCaloriesBurned()
+
+    fun getAverageSpeed() = runDAO.getTotalAverageSpeed()
+
+    fun getTotalTimeInMillis() = runDAO.getTotalTimeInMillis()
+
+
+}
---
+++ app/src/main/java/com/androiddevs/runningappyt/ui/MainActivity.kt
@@ -0,0 +1,4 @@
+package com.androiddevs.runningappyt.ui
+
+class MainActivity {
+}
